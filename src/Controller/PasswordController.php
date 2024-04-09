c7f844c5bb1802f74c27db2df5cc59e3d3a6ba1b
Yoru-cyber <carlosmendez170210@gmail.com>
2024-04-09 15:35:47 -0400

    added delete, get by id endpoints

--- src/Controller/PasswordController.php
+++ src/Controller/PasswordController.php
@@ -12,7 +12,6 @@ use App\Entity\Password;
 use Symfony\Component\Serializer\Encoder\JsonEncoder;
 use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
 use Symfony\Component\Serializer\Serializer;
-use Symfony\Component\Serializer\SerializerInterface;
 
 class PasswordController extends AbstractController
 {
@@ -21,7 +20,7 @@ class PasswordController extends AbstractController
     public $serializer;
     private $encoders;
     private $normalizers;
-
+    //add DI for serializer in Controller
     public function __construct(EntityManagerInterface $entityManager)
     {
         $this->entityManager = $entityManager;
@@ -31,6 +30,7 @@ class PasswordController extends AbstractController
 
         $this->serializer = new Serializer($this->normalizers, $this->encoders);
     }
+    //http:localhost:8000/password GET
     #[Route('/password', name: 'app_password', methods: ['GET'])]
     public function index(): JsonResponse
     {
@@ -42,6 +42,7 @@ class PasswordController extends AbstractController
             json_decode($this->serializer->serialize($list, 'json'))
         );
     }
+    //http:localhost:8000/password POST
     #[Route('/password', name: 'app_create_password', methods: ['POST'])]
     public function createPassword(Request $request)
     {
@@ -55,7 +56,29 @@ class PasswordController extends AbstractController
         } else {
             return $this->json(['Message' => 'Invalid request'], status: 400);
         }
-
-
+    }
+    //http:localhost:8000/password/1 DELETE
+    #[Route('/password/{id}', name: 'app_delete_password', methods: ['DELETE'])]
+    public function deletePassword(int $id)
+    {
+        $password = $this->repository->find($id);
+        if ($password != null) {
+            $this->entityManager->remove($password);
+            $this->entityManager->flush();
+            return $this->json(['Message' => 'Password removed']);
+        } else {
+            return $this->json(['Message' => 'Password does not exist'], status: 400);
+        }
+    }
+    //http:localhost:8000/password/1 GET
+    #[Route('/password/{id}', name: 'app_get_password', methods: ['GET'])]
+    public function getPassword(int $id)
+    {
+        $password = $this->repository->find($id);
+        if ($password != null) {
+            return $this->json($password);
+        } else {
+            return $this->json(['Message' => 'Password does not exist'], status: 400);
+        }
     }
 }
